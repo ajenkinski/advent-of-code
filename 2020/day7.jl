72209350aec5869873fbc3f2720db332c75d301a
ajenkinski <adam@thejenkins.org>
2020-12-08 12:53:27 -0500

    Completed day 7

--- 2020/day7.jl
+++ 2020/day7.jl
@@ -5,64 +5,65 @@ https://adventofcode.com/2020/day/7
 # Represent input as a directed graph with weighted edges.  Node names are the bag colors, 
 # edge weights are required number of bags.  Node A has an edge to B if bag color A contains one or more Bs.
 
-using SparseArrays
+using LightGraphs: SimpleDiGraph, add_edge!, edges, dfs_tree, neighbors
+using MetaGraphs: MetaDiGraph, set_prop!, set_indexing_prop!, get_prop
 
-struct Edge
-  from_node::String
-  to_node::String
-  count::Int
-end
-
-nodes = map(readlines("day7-input.txt")) do line
-  m = match(r"^(.+) bags contain (.+)$", line)
-  @assert m != nothing
-  node_color = m.captures[1]
-  
-  ngbrs_str = m.captures[2]
-  ngbrs = map(eachmatch(r"(\d+) (.*?) bag", ngbrs_str)) do ngbr_m
-    Edge(node_color, ngbr_m.captures[2], parse(Int, ngbr_m.captures[1]))
+graph = let lines = readlines("day7-input.txt")
+  nodes = map(lines) do line
+    m = match(r"^(.+) bags contain (.+)$", line)
+    @assert m != nothing
+    m.captures
   end
 
-  return (node_color, ngbrs)
-end
+  label_to_idx = Dict(map(((i, node),) -> (node[1], i), enumerate(nodes)))
 
-# map node label to indexes
-label_to_idx = Dict(map(((i, node),) -> (node[1], i), enumerate(nodes)))
+  g = MetaDiGraph(SimpleDiGraph(length(nodes)))
 
-# Represent graph as a sparse matrix, where g[r, c] being non-zero means there's an edge from node r to node c,
-# or for this problem, it means a bag of color r contains one or more bags of color c.
-graph = let edges = Iterators.flatten(map(n -> n[2], nodes))
-  sparse(map(e -> label_to_idx[e.from_node], edges), # row indexes
-         map(e -> label_to_idx[e.to_node], edges), # column indexes
-         map(e -> e.count, edges), # values
-         length(nodes), length(nodes))
+  for (label, ngbrs_str) in nodes
+    node_idx = label_to_idx[label]
+    set_prop!(g, node_idx, :color, label)
+
+    for m in eachmatch(r"(\d+) (.*?) bag", ngbrs_str)
+      add_edge!(g, node_idx, label_to_idx[m.captures[2]], :weight, parse(Int, m.captures[1]))
+    end
+  end
+
+  # Makes it possible to map from color name to node index
+  set_indexing_prop!(g, :color)
+  g
 end
 
 "Return an array of node indexes of all nodes in graph g reachable from node start"
-function reachable_nodes(g, start)
-  reachable = Set{Int}()
-  to_visit = findnz(g[start, :])[1]
-
-  while !isempty(to_visit)
-    union!(reachable, to_visit)
-    to_visit = setdiff(findnz(g[to_visit, :])[2], reachable)
-  en
-
-  return reachable
+function reachable_nodes(g, start_color)
+  start_idx = g[start_color, :color]
+  return unique(map(e -> e.dst, edges(dfs_tree(g, start_idx))))
 end
 
 
 # Part 1 asks to how many different bag colors can directly or indirectly contain a "shiny gold" bag.
 # For this I want to find all ancestor nodes of the shiny gold node.  Since my graph edges express the "contains" relation, I'll 
-# transpose the graph so the edges express "contained by".
+# reverse the graph so the edges express "contained by".
 
-part1_node_ids = reachable_nodes(transpose(graph), label_to_idx["shiny gold"])
+part1_node_ids = reachable_nodes(reverse(graph), "shiny gold")
 print("Part 1: shiny gold is contained by $(length(part1_node_ids)) bags\n")
 
 
-# Part 2 requires counting how many bags are in a "shiny gold" bag, or in graph terms, finding all reachable nodes and summing
-# the edge weights along the way.
+# Part 2 requires counting how many bags are in a "shiny gold" bag, or in graph terms, finding all reachable nodes and finding the
+# sum of the cumulative product of weights along each path.
 
-part2_node_ids = reachable_nodes(graph, label_to_idx["shiny gold"])
-print("Part 2: shiny gold contains $(length(part2_node_ids)) bags\n")
+"Counts number of bags directly or indirectly contained by the bag represented by start_node"
+function count_bags(g, start_node)
+  ngbrs = neighbors(g, start_node)
+  if isempty(ngbrs)
+    return 0
+  else
+    return sum(ngbrs) do ngbr
+      nbags = get_prop(g, start_node, ngbr, :weight)
+      nbags + nbags * count_bags(g, ngbr)
+    end
+  end
+end
+
+part2_answer = count_bags(graph, graph["shiny gold", :color])
+print("Part 2: num bags in shiny gold bag = $(part2_answer)\n")
 
