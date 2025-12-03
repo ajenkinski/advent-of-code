26ece028f1d3a0b4208bf1831ce3b1bfb770fe7d
ajenkinski <adam@thejenkins.org>
2025-12-03 09:59:05 -0500

    Day 3 part 2

--- 2025/day3.py
+++ 2025/day3.py
@@ -1,27 +1,28 @@
 from utils import read_input_file
 
 
-def solve_part1(banks: list[str]) -> int:
+def solve(banks: list[str], num_digits: int) -> int:
     """
-    Each bank consists of a string a digits. For each bank, find the largest two
-    digit number that can be made by choosing two digits from the bank, preserving
+    Each bank consists of a string a digits. For each bank, find the largest
+    number that can be made by choosing num_digits digits from the bank, preserving
     their order.  Return the sum of these numbers.
     """
 
     answer = 0
 
     for bank in banks:
-        first_digit = 0
-        second_digit = 0
+        digits = ["0"] * num_digits
+        bank_len = len(bank)
 
-        for i, digit in enumerate(map(int, bank)):
-            if i < (len(bank) - 1) and digit > first_digit:
-                first_digit = digit
-                second_digit = 0
-            else:
-                second_digit = max(second_digit, digit)
+        for i, digit in enumerate(bank):
+            for j in range(len(digits)):
+                if i < (bank_len - (num_digits - j - 1)) and digit > digits[j]:
+                    digits[j] = digit
+                    for k in range(j + 1, len(digits)):
+                        digits[k] = "0"
+                    break
 
-        answer += first_digit * 10 + second_digit
+        answer += int("".join(digits))
 
     return answer
 
@@ -29,8 +30,11 @@ def solve_part1(banks: list[str]) -> int:
 def main():
     banks = list(read_input_file("day3-input.txt").splitlines())
 
-    part1_solution = solve_part1(banks)
+    part1_solution = solve(banks, 2)
     print(f"Part 1 solution = {part1_solution}")
+
+    part2_solution = solve(banks, 12)
+    print(f"Part 2 solution = {part2_solution}")
 
 
 if __name__ == "__main__":
