233fdcc12f72f1ee428ca1ceeee4f4ccfea315cc
ajenkinski <adam@thejenkins.org>
2025-12-04 22:15:52 -0500

    Day 1 in Elixir

---
+++ 2025-exs/day1.exs
@@ -0,0 +1,58 @@
+defmodule Day1 do
+  @doc """
+  Parse input to a list, with left rotations as negative numbers and right rotations
+  as positive numbers.
+  """
+  def parse_input(txt) do
+    for line <- String.split(txt),
+        <<dir, num_str::binary>> = line,
+        num = String.to_integer(num_str) do
+      if dir == ?L do
+        -num
+      else
+        num
+      end
+    end
+  end
+
+  def solve_part1(rotations) do
+    {num_zeros, _} =
+      Enum.reduce(rotations, {0, 50}, fn rotation, {num_zeros, position} ->
+        position = Integer.mod(position + rotation, 100)
+
+        {if position == 0 do
+           num_zeros + 1
+         else
+           num_zeros
+         end, position}
+      end)
+
+    num_zeros
+  end
+
+  def solve_part2(rotations) do
+    {num_zeros, _} =
+      Enum.reduce(rotations, {0, 50}, fn rotation, {num_zeros, position} ->
+        num_zeros =
+          if rotation > 0 do
+            num_zeros + div(position + rotation, 100)
+          else
+            num_zeros + div(Integer.mod(100 - position, 100) - rotation, 100)
+          end
+
+        position = Integer.mod(position + rotation, 100)
+        {num_zeros, position}
+      end)
+
+    num_zeros
+  end
+end
+
+{:ok, input} = File.read("inputs/day1-input.txt")
+rotations = Day1.parse_input(input)
+
+part1_solution = Day1.solve_part1(rotations)
+IO.puts("Part 1 solution = #{part1_solution}")
+
+part2_solution = Day1.solve_part2(rotations)
+IO.puts("Part 2 solution = #{part2_solution}")
