31534da7da8bad6af07468b50e429e6934061a38
ajenkinski <adam@thejenkins.org>
2025-12-08 23:58:53 -0500

    Day 8 part 2

--- 2025-exs/day8.exs
+++ 2025-exs/day8.exs
@@ -3,14 +3,7 @@ defmodule Day8 do
     defstruct x: 0, y: 0, z: 0
   end
 
-  def parse_input(txt) do
-    String.trim(txt)
-    |> String.split()
-    |> Enum.map(fn line ->
-      [x, y, z] = String.split(line, ",") |> Enum.map(&String.to_integer/1)
-      %Point{x: x, y: y, z: z}
-    end)
-  end
+  defstruct points: [], pair_distances: []
 
   defp distance(p1, p2) do
     ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2 + (p1.z - p2.z) ** 2) ** 0.5
@@ -26,17 +19,29 @@ defmodule Day8 do
     sublists ++ n_combinations(n, t)
   end
 
-  def solve_part1(points) do
+  def parse_input(txt) do
+    points =
+      String.trim(txt)
+      |> String.split()
+      |> Enum.map(fn line ->
+        [x, y, z] = String.split(line, ",") |> Enum.map(&String.to_integer/1)
+        %Point{x: x, y: y, z: z}
+      end)
+
     pair_distances =
       for [p1, p2] <- n_combinations(2, points) do
         {distance(p1, p2), {p1, p2}}
       end
       |> Enum.sort(fn {d1, _}, {d2, _} -> d1 <= d2 end)
 
+    %Day8{points: points, pair_distances: pair_distances}
+  end
+
+  def solve_part1(input) do
     g =
       Graph.new(type: :undirected)
       |> Graph.add_edges(
-        for {_, {p1, p2}} <- Stream.take(pair_distances, 1000) do
+        for {_, {p1, p2}} <- Stream.take(input.pair_distances, 1000) do
           {p1, p2}
         end
       )
@@ -47,6 +52,36 @@ defmodule Day8 do
     |> Enum.take(3)
     |> Enum.product()
   end
+
+  def solve_part2(input) do
+    num_points = Enum.count(input.points)
+
+    {p1, p2} =
+      Enum.reduce_while(input.pair_distances, [], fn {_, {p1, p2}}, groups ->
+        {with, without} =
+          Enum.split_with(groups, fn g -> MapSet.member?(g, p1) or MapSet.member?(g, p2) end)
+
+        with =
+          if Enum.empty?(with) do
+            MapSet.new([p1, p2])
+          else
+            Enum.reduce(with, &MapSet.union/2) |> MapSet.put(p1) |> MapSet.put(p2)
+          end
+
+        if MapSet.size(with) == num_points do
+          {:halt, {p1, p2}}
+        else
+          {:cont, [with | without]}
+        end
+      end)
+
+    p1.x * p2.x
+  end
 end
 
-Aoc2025.Utils.run_day("day8-input.txt", &Day8.parse_input/1, &Day8.solve_part1/1)
+Aoc2025.Utils.run_day(
+  "day8-input.txt",
+  &Day8.parse_input/1,
+  &Day8.solve_part1/1,
+  &Day8.solve_part2/1
+)
